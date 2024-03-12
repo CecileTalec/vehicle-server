8bf36702c8ea3db7fba95d88d32a2c4e86132b93
VictorienO Oll <victorien.ollivier@ensg.eu>
2024-03-12 13:57:40 +0000

    Correction de Delete

--- vehicle/delete.go
+++ vehicle/delete.go
@@ -1,12 +1,13 @@
 package vehicle
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
-	"fmt"
+
+	"github.com/CecileTalec/vehicle-server/pkg/httputil"
 	"github.com/CecileTalec/vehicle-server/storage"
 	"go.uber.org/zap"
-	"../vehiclestore/store.go"
 )
 
 type DeleteHandler struct {
@@ -22,16 +23,31 @@ func NewDeleteHandler(store storage.Store, logger *zap.Logger) *DeleteHandler {
 }
 
 func (d *DeleteHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	http.Error(rw, "Not Implemented", http.StatusInternalServerError)
 	id := r.PathValue("id")
-	id_num, err := strconv.ParseInt(id, 10, 64);
+	id_num, err := strconv.ParseInt(id, 10, 64)
 	if err == nil {
-		fmt.Printf("%T, %v\n", id_num, err) //cas erreur
+		d.logger.Error(
+			"Could not decode request body",
+			zap.Error(err),
+		)
+		httputil.ServeError(rw, http.StatusBadRequest, err)
+		return
 	}
-	reponse := d.store.Delete(r.Context, id_num)
-	if !reponse{
+
+	reponse, err := d.store.Vehicle().Delete(r.Context(), id_num)
+	if err == nil {
+		d.logger.Error(
+			"Could not decode request body",
+			zap.Error(err),
+		)
+		httputil.ServeError(rw, http.StatusBadRequest, err)
+		return
+	}
+
+	if !reponse {
 		fmt.Println("ERREUR 404")
 	}
+
 	rw.WriteHeader(http.StatusNoContent)
 
 }
