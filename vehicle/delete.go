7ac849fe3201bcdff102ebb86043e89fd89ebd84
VictorienO Oll <victorien.ollivier@ensg.eu>
2024-03-12 13:47:24 +0000

    J'essaie de compiler

--- vehicle/delete.go
+++ vehicle/delete.go
@@ -2,9 +2,11 @@ package vehicle
 
 import (
 	"net/http"
-
+	"strconv"
+	"fmt"
 	"github.com/CecileTalec/vehicle-server/storage"
 	"go.uber.org/zap"
+	"../vehiclestore/store.go"
 )
 
 type DeleteHandler struct {
@@ -21,4 +23,15 @@ func NewDeleteHandler(store storage.Store, logger *zap.Logger) *DeleteHandler {
 
 func (d *DeleteHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	http.Error(rw, "Not Implemented", http.StatusInternalServerError)
+	id := r.PathValue("id")
+	id_num, err := strconv.ParseInt(id, 10, 64);
+	if err == nil {
+		fmt.Printf("%T, %v\n", id_num, err) //cas erreur
+	}
+	reponse := d.store.Delete(r.Context, id_num)
+	if !reponse{
+		fmt.Println("ERREUR 404")
+	}
+	rw.WriteHeader(http.StatusNoContent)
+
 }
